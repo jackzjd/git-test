b9ab59c59ab4b203d5f67d5b9b7f6af3ccc145d4
‘1498150486@qq.com’ <1498150486@qq.com>
2023-03-29 09:04:20 +0800

    second commit

--- src/main/java/com/cumt/GitTest.java
+++ src/main/java/com/cumt/GitTest.java
@@ -1,7 +1,9 @@
 package com.cumt;
 
 public class GitTest {
+    // 蓝色表示被追踪过 但是被修改了
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
