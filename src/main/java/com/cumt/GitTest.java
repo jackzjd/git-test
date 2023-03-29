7c3f91c87dfccf0dfe73af7ef45cd6956a9d627f
‘1498150486@qq.com’ <1498150486@qq.com>
2023-03-29 09:06:21 +0800

    third commit

--- src/main/java/com/cumt/GitTest.java
+++ src/main/java/com/cumt/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
+        // 已经被追踪的文件 可以不用提交暂存区 可以直接提交本地库
     }
 }
